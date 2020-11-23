05da31e6abf944b7c911dd1e3c2be8c595e6d5e6
Jose_Martinez <al0277334@tecmilenio.mx>
2020-11-23 15:34:12 -0600

    Adding List

---
+++ src/com/tecmilenio/ormuz/Main.java
@@ -0,0 +1,21 @@
+package com.tecmilenio.ormuz;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("Kaan");
+        myList.add("The wire");
+        myList.add( "Dragon Ball");
+        myList.add("Discord");
+        myList.add("Tacos Pony");
+        myList.add("Leon");
+        myList.add("Adrian");
+        myList.add("Ronaldo");
+        myList.add("WWI");
+        myList.add("Xoloitzcuintle");
+        
+    }
+}
